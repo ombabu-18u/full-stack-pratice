57717946fbb4310711004439df4a107d8175d840
Ombabu chaurasiya <ombabuchaurasiya@Ombabus-MacBook-Air.local>
2026-02-15 14:18:25 +0530

    second-pratice

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 
 
+// const App = () => {
+//   let x = "ombabu";
+//     let array =["user1","user2","user3","user4","user5"];
+//   return (
+//     <div>
+//      {x}
+//      {array.map((user) =>{return <h2>{user}</h2>})}
+//     </div>
+//   )
+// }
+
+// export default App
+
+
 const App = () => {
-  let x = "ombabu";
-    let array =["user1","user2","user3","user4","user5"];
+
+  let data = "girls";
   return (
     <div>
-     {x}
-     {array.map((user) =>{return <h2>{user}</h2>})}
+      {data === "girls" ? <h1>Girls</h1> : <h1>Boys</h1>}
     </div>
   )
 }
