a5b230b00a1f6f47054d2e239f8d03d8a05ec35e
Ombabu chaurasiya <ombabuchaurasiya@Ombabus-MacBook-Air.local>
2026-02-15 12:24:36 +0530

    first-project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+const App = () => {
+  let x = "ombabu";
+    let array =["user1","user2","user3","user4","user5"];
+  return (
+    <div>
+     {x}
+     {array.map((user) =>{return <h2>{user}</h2>})}
+    </div>
+  )
+}
+
+export default App
